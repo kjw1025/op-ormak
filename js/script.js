74835215af44d4d14fbf9c8c4416af24ceda94c3
unknown <di1025p@gmail.com>
2022-07-07 15:00:23 +0900

    메뉴 동작 작업

--- js/script.js
+++ js/script.js
@@ -22,8 +22,40 @@ window.onload = function () {
     pagination: {
       el: '.sw-visual-pg',
     }
-
   });
 
 
-}
+  // 메뉴 열기 기능
+  $('.l-top-menu').click(function (e) {
+    e.preventDefault();
+    $('.maingnb').toggleClass('maingnb-open');
+    $('.menu-dim').toggleClass('menu-dim-open');
+  });
+
+  // 메뉴 닫기 기능
+  $('.close-bt').click(function (e) {
+    e.preventDefault();
+    $('.maingnb').removeClass('maingnb-open');
+    $('.menu-dim').removeClass('menu-dim-open');
+  });
+
+  $('.menu-dim').click(function (e) {
+    e.preventDefault();
+    $('.maingnb').removeClass('maingnb-open');
+    $('.menu-dim').removeClass('menu-dim-open');
+  });
+};
+
+
+
+
+// 모바일 메뉴 기능
+// .mb-bt 를 저장해서 활용하자.
+// $('.mb-bt').click(function (e) {
+//   e.preventDefault();
+//   $(this).toggleClass('mb-bt-open');
+//   $('.mb-dim').toggleClass('mb-dim-open');
+//   $('.mb-wrap').toggleClass('mb-wrap-open');
+// });
+
+// $('.mb-bt').removeClass('mb-bt-open');
